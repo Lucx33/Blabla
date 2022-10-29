85dbb243c5dcb05144dd48692be3547bb20562e4
Lucx33 <lucaolima33@gmail.com>
2022-10-29 14:44:14 -0300

    Atualizando

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(7), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
+
+@app.route('/')
+def home():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
