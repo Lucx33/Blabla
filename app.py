a10cd64c81fb58814a2a588e2d5b226bf6ffb9d0
Lucx33 <104513017+Lucx33@users.noreply.github.com>
2022-11-08 22:21:37 -0300

    Foi

--- app.py
+++ app.py
@@ -12,24 +12,34 @@ bcrypt = Bcrypt(app)
 
 
 
-client = pymongo.MongoClient("mongodb+srv://pstud:gVJQTsM2ftVKES5d@inf1039cardapuc.1cskrne.mongodb.net/?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://pstud:gVJQTsM2ftVKES5d@inf1039cardapuc.1cskrne.mongodb.net/?retryWrites=true&w=majority&authSource=admin")
 
 db = client.cardapuc
 
-col = db.restaurantes
+col = db.usuarios
 
 
 @app.route("/", methods=["POST", "GET"])
+@app.route("/login/", methods=["POST", "GET"])
 def landing_page():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
+        
         result = col.find_one({"username": username})
-        try:
+        if result is not None:
+            pw_unhash = bcrypt.check_password_hash(result["password"], password)
+            if pw_unhash == False:
+                return render_template("failure.html")
+            else:
+                return "<p>login</p>"
+
+        else:
             pw_hash = bcrypt.generate_password_hash(password)
-            col.find_one({"password": pw_hash})
-        except:
-            abort(404)
-        return render_template("register.html", result = result)
+            col.insert_one({"username": username, "password": pw_hash}) 
+        
+        return "<p>Success</p>"# render_template("register.html")
     
     return render_template("register.html")
+
+
